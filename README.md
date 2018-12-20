8da104cec449738d0f9a855bb1eba2af8adf6f49
Anton-Cao <cao.anton@gmail.com>
2018-12-19 23:12:42 -0500

    Add functionality for running scripts

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Ship Git
 Simple Flask web app to automatically pull updates on a remote server. Inspired by [Git-Auto-Deploy](https://github.com/olipo186/Git-Auto-Deploy).
+
+## How does it work?
+In `scripts/`, whenever you push to `GITHUB_REPO`, the bash script `GITHUB_REPO.sh` will be run. For each `GITHUB_REPO` you wish to use Ship Git with, you can create a new executable. Make sure the file has the correct permissions to run: `chmod 755 GITHUB_REPO.sh`.
+
+## Deploying
+1. Clone repository onto the server hosting your websites
+1. Create and activate python3 virtual environment
+1. `cp sample-config.json config.json`
+1. Edit config file as needed
+1. Run the app
+1. Make sure the app is available through some URL
+1. Add the URL to the list of GitHub webhooks in your repository
--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, request
+import json
+import subprocess
 
 app = Flask(__name__)
 app.config.from_json("config.json")
 
+def get_repo(content):
+    return content["repository"]["name"]
+
+def execute_script(repo):
+    try:
+        filename = f"./scripts/{repo}.sh"
+        subprocess.call(filename, shell=True)
+    except Exception as e:
+        print(e)
+
 @app.route('/', methods=["POST"])
 def handler():
-    pass
+    if request.is_json:
+        content = request.get_json()
+    else:
+        content = json.loads(request.form.get("payload"))
+    try:
+        repository = get_repo(content)
+        execute_script(repository)
+        return "success", 200
+    except: # repository not found
+        err_msg = f"repository {repository} not found"
+        print(err_msg)
+        return err_msg, 500
 
 if __name__ == "__main__":
     app.run(port=app.config["PORT"])
